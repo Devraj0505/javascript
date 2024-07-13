249075522f0ed2837fc9dc34f1e7ba2d850ebbdd
Devraj0505 <devrajroy050@gmail.com>
2024-07-13 10:15:09 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,20 @@
+const account =14432
+let accountEmail ="devraj21@gmail.com"
+var accountPassword ="231"
+accountcity = "jaipur"
+
+let accountstate
+// will intialize value undefined if the value is not given 
+
+accountEmail ="dev21@gmail.com"
+accountPassword ="3231"
+accountcity = "bengalore"
+
+// account = 3 //not allowed
+console.log(account)
+
+console.table([accountEmail, accountPassword,accountcity,accountstate])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
