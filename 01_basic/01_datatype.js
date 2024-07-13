d554f662ab418be57359489604d7ee4f075182b6
Devraj0505 <devrajroy050@gmail.com>
2024-07-13 10:36:58 +0000

    datatype

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //WE ARE USING NODEJS , NOT BROWSER THIS WORKS ON BROWSER 
+
+//code redability 
+
+let name ="hitesh"//string datatype
+let age = 18 //number
+let isLoggedIN = false //boolean 
+let state
+//number =>2^53 
+//bigint 
+// string =>""
+// boolean => true/false
+// null =>stand alone value
+// undefined => not yet assigned value 
+//symbol =>unique
+
+// object 
+console.log(typeof null);
+
+
+// null type is object 
+// undefined type is undefined
